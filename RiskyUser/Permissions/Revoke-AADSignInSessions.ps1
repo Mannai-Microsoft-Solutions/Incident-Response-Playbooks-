1aff938f32ad40b309956e8abca38d9793cd30bb
rakeshprasadMannai <141730662+rakeshprasadMannai@users.noreply.github.com>
2024-01-10 20:34:17 +0530

    Update Revoke-AADSignInSessions.ps1

--- RiskyUser/Permissions/Revoke-AADSignInSessions.ps1
+++ RiskyUser/Permissions/Revoke-AADSignInSessions.ps1
@@ -10,3 +10,6 @@ $PermissionName = "User.ReadWrite.All"
 $GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAppId'"
 $AppRole = $GraphServicePrincipal.AppRoles | Where-Object {$_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application"}
 New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole.Id
+
+$RoleName = "Microsoft Sentinel Responder"
+New-AzRoleAssignment -ObjectId $MIGuid -RoleDefinitionName $RoleName -Scope /subscriptions/$SubscriptionId/resourcegroups/$ResourceGroup
