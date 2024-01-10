e123277a7c56e089a57fde2b7b2537224bd06584
rakeshprasadMannai <141730662+rakeshprasadMannai@users.noreply.github.com>
2024-01-10 20:33:50 +0530

    Update Block-AADUserOrAdmin.ps1

--- RiskyUser/Permissions/Block-AADUserOrAdmin.ps1
+++ RiskyUser/Permissions/Block-AADUserOrAdmin.ps1
@@ -16,20 +16,16 @@ $GraphServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$GraphAp
 
 $AppRole1 = $GraphServicePrincipal.AppRoles | Where-Object { $_.Value -eq $PermissionName1 -and $_.AllowedMemberTypes -contains "Application" }
 
-New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
-    -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole1.Id
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole1.Id
 
 $AppRole2 = $GraphServicePrincipal.AppRoles | Where-Object { $_.Value -eq $PermissionName2 -and $_.AllowedMemberTypes -contains "Application" }
-New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
-    -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole2.Id
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole2.Id
 
 $AppRole3 = $GraphServicePrincipal.AppRoles | Where-Object { $_.Value -eq $PermissionName3 -and $_.AllowedMemberTypes -contains "Application" }
-New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
-    -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole3.Id
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole3.Id
 
 $AppRole4 = $GraphServicePrincipal.AppRoles | Where-Object { $_.Value -eq $PermissionName4 -and $_.AllowedMemberTypes -contains "Application" }
-New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId `
-    -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole4.Id
+New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId -ResourceId $GraphServicePrincipal.ObjectId -Id $AppRole4.Id
 
 $RoleName = "Microsoft Sentinel Responder"
 New-AzRoleAssignment -ObjectId $MIGuid -RoleDefinitionName $RoleName -Scope /subscriptions/$SubscriptionId/resourcegroups/$ResourceGroup
