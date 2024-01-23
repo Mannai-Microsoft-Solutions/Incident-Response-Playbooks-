09d2442a230092eac9b86cec5f14b6e2b3ef7718
rakeshprasadMannai <141730662+rakeshprasadMannai@users.noreply.github.com>
2024-01-23 16:23:06 +0530

    Update Restrict-MDEDomain.ps1

--- RestrictEntities/Permissions/Restrict-MDEDomain.ps1
+++ RestrictEntities/Permissions/Restrict-MDEDomain.ps1
@@ -1,4 +1,6 @@
 param(
+    [Parameter(Mandatory = $true)]$SubscriptionId,
+    [Parameter(Mandatory = $true)]$ResourceGroup,
     [Parameter(Mandatory = $true)]$MIGuid
 )
 
@@ -6,7 +8,10 @@ $MI = Get-AzureADServicePrincipal -ObjectId $MIGuid
 
 $MDEAppId = "fc780465-2017-40d4-a0c5-307022471b92"
 $PermissionName = "Ti.ReadWrite" 
+$RoleName = "Microsoft Sentinel Responder"
 
 $MDEServicePrincipal = Get-AzureADServicePrincipal -Filter "appId eq '$MDEAppId'"
 $AppRole = $MDEServicePrincipal.AppRoles | Where-Object { $_.Value -eq $PermissionName -and $_.AllowedMemberTypes -contains "Application" }
 New-AzureAdServiceAppRoleAssignment -ObjectId $MI.ObjectId -PrincipalId $MI.ObjectId -ResourceId $MDEServicePrincipal.ObjectId -Id $AppRole.Id
+
+New-AzRoleAssignment -ObjectId $MIGuid -RoleDefinitionName $RoleName -Scope /subscriptions/$SubscriptionId/resourcegroups/$ResourceGroup
